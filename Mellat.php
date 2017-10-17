92e3dd3846a86c12ee972778dd443730bd3fdb04
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2017-10-17 11:14:54 +0330

    v1.0 beta

---
+++ Mellat.php
@@ -0,0 +1,181 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/*
+ * CodeIgniter Mellat getway library
+ *
+ * @author              Mahdi Majidzadeh (http://majidzadeh.ir)
+ * @license             GNU Public License 2.0
+ * @package             Mellat
+ */
+
+if (!class_exists('nusoap_client')) {
+    require_once 'nusoap/nusoap.php';
+}
+class Mellat {
+
+    private $terminal = NULL;
+    private $username = NULL;
+    private $password = NULL;
+    private $amount = NULL;
+    private $callback = NULL;
+    private $order = NULL;
+
+    private $url_pay = 'http://interfaces.core.sw.bps.com/';
+    private $url_wsdl = 'https://bpm.shaparak.ir/pgwchannel/services/pgw?wsdl';
+
+    public function set_options($terminal = '', $username = '', $password = '', $amount = 0, $order = 0, $callback = '')
+    {
+        if(!empty($terminal))
+            $this->terminal = $terminal;
+
+        if(!empty($username))
+            $this->username = $username;
+
+        if(!empty($password))
+            $this->password = $password;
+
+        if(!empty($amount))
+            $this->amount = $amount;
+
+        if(!empty($order))
+            $this->order = $order;
+
+        if(!empty($callback))
+            $this->callback = $callback;
+    }
+
+    public function send()
+    {
+        $client             = new nusoap_client($this->url_wsdl, 'wsdl');
+        $terminalId         = $this->terminal;
+        $userName           = $this->username;
+        $userPassword       = $this->password;
+        $orderId            = $this->order;
+        $amount             = $this->amount * 10;
+        $callBackUrl        = $this->callback;
+        $localDate          = date('Ymd');
+        $localTime          = date('His');
+        $additionalData     = '';
+        $payerId = 0;
+
+        $err = $client->getError();
+        if ($err) {
+            return false;
+        }
+
+        $parameters = array(
+            'terminalId'        => $terminalId,
+            'userName'          => $userName,
+            'userPassword'      => $userPassword,
+            'orderId'           => $orderId,
+            'amount'            => $amount,
+            'localDate'         => $localDate,
+            'localTime'         => $localTime,
+            'additionalData'    => $additionalData,
+            'callBackUrl'       => $callBackUrl,
+            'payerId'           => $payerId
+        );
+        $result = $client->call('bpPayRequest', $parameters, $this->$url_pay);
+
+        if ($client->fault) {
+            return false;
+        }
+        else {
+            $resultStr  = $result;
+            $err = $client->getError();
+            if ($err) {
+                return false;
+            }
+            else {
+                foreach ($resultStr as $value) {
+                    $resultStr = $value;
+                }
+                $res = explode (',',$resultStr);
+                $ResCode = $res[0]; 
+                if ($ResCode == "0") {
+                    $this->postRefId($res[1]);
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    protected function verify_payment($params){
+        $client                     = new nusoap_client( $this->url_wsdl,'wsdl') ;
+        $orderId                    = $params["SaleOrderId"];
+        $verifySaleOrderId          = $params["SaleOrderId"];
+        $verifySaleReferenceId      = $params['SaleReferenceId'];
+        $err = $client->getError();
+        if ($err) {
+            return false;
+        }
+        $parameters = array(
+            'terminalId'            => $this->terminal,
+            'userName'              => $this->username,
+            'userPassword'          => $this->password,
+            'orderId'               => $orderId,
+            'saleOrderId'           => $verifySaleOrderId,
+            'saleReferenceId'       => $verifySaleReferenceId
+        );
+        $result = $client->call('bpVerifyRequest', $parameters, $this->url_pay);
+        if ($client->fault) {
+            return false;
+        } 
+        else {
+            $resultStr = $result;
+            $err = $client->getError();
+            if ($err) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function postRefId($refIdValue){
+        echo '<script language="javascript" type="text/javascript"> 
+                function postRefId (refIdValue) {
+                var form = document.createElement("form");
+                form.setAttribute("method", "POST");
+                form.setAttribute("action", "https://bpm.shaparak.ir/pgwchannel/startpay.mellat");         
+                form.setAttribute("target", "_self");
+                var hiddenField = document.createElement("input");              
+                hiddenField.setAttribute("name", "RefId");
+                hiddenField.setAttribute("value", refIdValue);
+                form.appendChild(hiddenField);
+                document.body.appendChild(form);         
+                form.submit();
+                document.body.removeChild(form);
+            }
+            postRefId("' . $refIdValue . '");
+            </script>';
+    }
+
+    protected function error($number){
+        return false;
+    }
+
+    public function get($params){
+        if( $params["ResCode"] == 0 ){
+            if( $this->verify_payment($params) == true ) {
+                if( $this->settle_payment($params) == true ) {
+                    return array(
+                        "status"            =>  "success", 
+                        "ResCode"           =>  $params["ResCode"], 
+                        "RefId"             =>  $params["RefId"], 
+                        "SaleOrderId"       =>  $params["SaleOrderId"], 
+                        "SaleReferenceId"   =>  $params["SaleReferenceId"]
+                    );
+                }
+            }
+        }
+        return false;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-CodeIgniter-Mellat-gateway
+# CodeIgniter-Zarinpal-gateway
+[![StyleCI](https://styleci.io/repos/57077375/shield)](https://styleci.io/repos/57077375)
+
+Codeigniter library for Iranian gateway, [Zarinpal](https://www.zarinpal.com/)
+
+##how to install
+Copy `Zarinpal.php` and `nusoap.php` to `application/libraries` of your own project.
+
+##how to use
+First, load library:
+```
+$this->load->library('zarinpal');
+```
+
+For sending user to gateway:
+```
+$this->zarinpal->request($merchant_id , $amount, $desc, $call_back, $mobile, $email);
+```
+Full code is:
+```
+if($this->zarinpal->request($merchant_id , $amount, $desc, $call_back, $mobile, $email)){
+    $authority = $this->zarinpal->getAuthority();
+    // do database 
+    $this->zarinpal->redirect();
+}
+else{
+    $error = $this->zarinpal->getError();
+}
+```
+For verify user payment:
+```
+$this->zarinpal->verify($merchant_id , $amount, $authority);
+```
+Full code is:
+```
+if($_GET['Status'] == 'OK'){
+    if($this->zarinpal->verify($merchant_id , $amount, $authority)){
+        $refid = $this->zarinpal->getRefId();
+        // do database 
+    }
+    else{
+        $error = $this->zarinpal->getError();
+    }
+}
+else{
+    //use cancel payment
+}
+```
+##Sandbox
+For test for script, you can turn on sandbox mode:
+```
+$this->zarinpal->sandbox();
+```
+
+##Contributor
+- Mahdi Majidzadeh ([github](https://github.com/MahdiMajidzadeh))
