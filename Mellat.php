8d3b18eec1aded727a733a2b5a118a7d76897cd7
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2017-10-22 12:14:45 +0000

    Apply fixes from StyleCI

--- Mellat.php
+++ Mellat.php
@@ -13,51 +13,57 @@ defined('BASEPATH') or exit('No direct script access allowed');
 if (!class_exists('nusoap_client')) {
     require_once 'nusoap/nusoap.php';
 }
-class Mellat {
-
-    private $terminal = NULL;
-    private $username = NULL;
-    private $password = NULL;
-    private $amount = NULL;
-    private $callback = NULL;
-    private $order = NULL;
+class Mellat
+{
+    private $terminal = null;
+    private $username = null;
+    private $password = null;
+    private $amount = null;
+    private $callback = null;
+    private $order = null;
 
     private $url_pay = 'http://interfaces.core.sw.bps.com/';
     private $url_wsdl = 'https://bpm.shaparak.ir/pgwchannel/services/pgw?wsdl';
 
     public function set_options($terminal = '', $username = '', $password = '', $amount = 0, $order = 0, $callback = '')
     {
-        if(!empty($terminal))
+        if (!empty($terminal)) {
             $this->terminal = $terminal;
+        }
 
-        if(!empty($username))
+        if (!empty($username)) {
             $this->username = $username;
+        }
 
-        if(!empty($password))
+        if (!empty($password)) {
             $this->password = $password;
+        }
 
-        if(!empty($amount))
+        if (!empty($amount)) {
             $this->amount = $amount;
+        }
 
-        if(!empty($order))
+        if (!empty($order)) {
             $this->order = $order;
+        }
 
-        if(!empty($callback))
+        if (!empty($callback)) {
             $this->callback = $callback;
+        }
     }
 
     public function call_bank()
     {
-        $client             = new nusoap_client($this->url_wsdl, 'wsdl');
-        $terminalId         = $this->terminal;
-        $userName           = $this->username;
-        $userPassword       = $this->password;
-        $orderId            = $this->order;
-        $amount             = $this->amount * 10;
-        $callBackUrl        = $this->callback;
-        $localDate          = date('Ymd');
-        $localTime          = date('His');
-        $additionalData     = '';
+        $client = new nusoap_client($this->url_wsdl, 'wsdl');
+        $terminalId = $this->terminal;
+        $userName = $this->username;
+        $userPassword = $this->password;
+        $orderId = $this->order;
+        $amount = $this->amount * 10;
+        $callBackUrl = $this->callback;
+        $localDate = date('Ymd');
+        $localTime = date('His');
+        $additionalData = '';
         $payerId = 0;
 
         $err = $client->getError();
@@ -65,7 +71,7 @@ class Mellat {
             return false;
         }
 
-        $parameters = array(
+        $parameters = [
             'terminalId'        => $terminalId,
             'userName'          => $userName,
             'userPassword'      => $userPassword,
@@ -75,66 +81,63 @@ class Mellat {
             'localTime'         => $localTime,
             'additionalData'    => $additionalData,
             'callBackUrl'       => $callBackUrl,
-            'payerId'           => $payerId
-        );
+            'payerId'           => $payerId,
+        ];
         $result = $client->call('bpPayRequest', $parameters, $this->url_pay);
 
         if ($client->fault) {
             return false;
-        }
-        else {
-            $resultStr  = $result;
+        } else {
+            $resultStr = $result;
             $err = $client->getError();
             if ($err) {
                 return false;
-            }
-            else {
+            } else {
                 foreach ($resultStr as $value) {
                     $resultStr = $value;
                 }
-                $res = explode (',',$resultStr);
-                $ResCode = $res[0]; 
-                if ($ResCode == "0") {
+                $res = explode(',', $resultStr);
+                $ResCode = $res[0];
+                if ($ResCode == '0') {
                     return $res[1];
-                }
-                else {
+                } else {
                     return false;
                 }
             }
         }
     }
 
-    public function verify_payment($SaleOrderId, $SaleReferenceId){
-        $client                     = new nusoap_client( $this->url_wsdl,'wsdl') ;
-        $orderId                    = $SaleOrderId;
-        $verifySaleOrderId          = $SaleOrderId;
-        $verifySaleReferenceId      = $SaleReferenceId;
+    public function verify_payment($SaleOrderId, $SaleReferenceId)
+    {
+        $client = new nusoap_client($this->url_wsdl, 'wsdl');
+        $orderId = $SaleOrderId;
+        $verifySaleOrderId = $SaleOrderId;
+        $verifySaleReferenceId = $SaleReferenceId;
         $err = $client->getError();
         if ($err) {
             return false;
         }
-        $parameters = array(
+        $parameters = [
             'terminalId'            => $this->terminal,
             'userName'              => $this->username,
             'userPassword'          => $this->password,
             'orderId'               => $orderId,
             'saleOrderId'           => $verifySaleOrderId,
-            'saleReferenceId'       => $verifySaleReferenceId
-        );
+            'saleReferenceId'       => $verifySaleReferenceId,
+        ];
         $result = $client->call('bpVerifyRequest', $parameters, $this->url_pay);
         if ($client->fault) {
             return false;
-        } 
-        else {
+        } else {
             $resultStr = $result;
             $err = $client->getError();
             if ($err) {
                 return false;
-            }
-            else {
+            } else {
                 return true;
             }
         }
+
         return false;
     }
 
@@ -154,28 +157,31 @@ class Mellat {
                 form.submit();
                 document.body.removeChild(form);
             }
-            postRefId("' . $refIdValue . '");
+            postRefId("'.$refIdValue.'");
             </script></html>';
     }
 
-    protected function error($number){
+    protected function error($number)
+    {
         return false;
     }
 
-    public function get($params){
-        if( $params["ResCode"] == 0 ){
-            if( $this->verify_payment($params) == true ) {
-                if( $this->settle_payment($params) == true ) {
-                    return array(
-                        "status"            =>  "success", 
-                        "ResCode"           =>  $params["ResCode"], 
-                        "RefId"             =>  $params["RefId"], 
-                        "SaleOrderId"       =>  $params["SaleOrderId"], 
-                        "SaleReferenceId"   =>  $params["SaleReferenceId"]
-                    );
+    public function get($params)
+    {
+        if ($params['ResCode'] == 0) {
+            if ($this->verify_payment($params) == true) {
+                if ($this->settle_payment($params) == true) {
+                    return [
+                        'status'            => 'success',
+                        'ResCode'           => $params['ResCode'],
+                        'RefId'             => $params['RefId'],
+                        'SaleOrderId'       => $params['SaleOrderId'],
+                        'SaleReferenceId'   => $params['SaleReferenceId'],
+                    ];
                 }
             }
         }
+
         return false;
     }
 }
